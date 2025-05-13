c5d4ccb7b79e3e604e703491f56f893bc31f278d
amJohnnyma <dewaldcoleskydc@gmail.com>
2025-05-13 21:47:14 +0200

    created neuron class

---
+++ myapp/src/main/java/com/Neuron.java
@@ -0,0 +1,47 @@
+package com;
+import java.util.*;
+
+class Neuron
+{
+
+    double[] weights; //weights to each othe
+    double bias;
+    double output;
+    double delta;
+
+    public Neuron(int inputSize) {
+        weights = new double[inputSize];
+        Random r = new Random();
+        for (int i = 0; i < inputSize; i++) { //small random weight for each input
+            weights[i] = r.nextGaussian() * 0.01; // small random init
+        }
+        bias = 0;
+
+    }
+
+    public double activate(double[] inputs) {
+        double z = bias + weightedSum(inputs);
+        output = sigmoid(z); 
+        return output;
+
+        /*
+            Sigmoid: σ(z)=11+e−zσ(z)=1+e−z1​
+            ReLU (Rectified Linear Unit): f(z)=max⁡(0,z)f(z)=max(0,z)
+            Tanh (Hyperbolic Tangent): tanh⁡(z)=21+e−2z–1tanh(z)=1+e−2z2​–1
+         */
+    }
+
+    public double weightedSum(double[] inputs)
+    {
+        double sum = bias;
+        for(int k =0; k < inputs.length;k++)
+        {
+            sum += weights[k] * inputs[k];
+        }
+        return sum;
+    }
+
+    private double sigmoid(double x) {
+        return 1.0 / (1.0 + Math.exp(-x));
+    }
+}
