9dc44b9ffbfffbfc456590a7ff99cfd9254031f7
NicohaasHerhaasus <hencopritor06@gmail.com>
2025-05-14 07:07:13 +0200

    Initial DT implemented accuracy still low, need to change outouts as well

--- myapp/src/main/java/com/Main.java
+++ myapp/src/main/java/com/Main.java
@@ -1,13 +1,15 @@
 package com;
-
+import main.java.com.CryptoStockPredictor;
 /**
  * Hello world!
  *
  */
-public class App 
+public class Main 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        CryptoStockPredictor DT = new CryptoStockPredictor();
+        DT.DT();
     }
 }
