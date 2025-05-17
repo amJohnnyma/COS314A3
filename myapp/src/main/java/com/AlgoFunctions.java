0c7910dc0f373b32f7912a83c66d37d9ba89c871
amJohnnyma <dewaldcoleskydc@gmail.com>
2025-05-17 18:36:20 +0200

    Removed unused imports

--- myapp/src/main/java/com/AlgoFunctions.java
+++ myapp/src/main/java/com/AlgoFunctions.java
@@ -6,47 +6,44 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.TimeUnit;
 
-
 public class AlgoFunctions {
 
-    public boolean runMLP(int iterations, int runs, double learningRate, long seed, int batchSize, int hiddenSize, int hiddenLayers, double targetAccuracy) {
-    AtomicInteger completed = new AtomicInteger(0);  // Shared thread-safe counter
+    public boolean runMLP(int iterations, int runs, double learningRate, long seed, int batchSize, int hiddenSize,
+            int hiddenLayers, double targetAccuracy) {
+        AtomicInteger completed = new AtomicInteger(0); // Shared thread-safe counter
 
         boolean useRandomSeeds = (seed == 0);
         final int numRuns;
-        if(!useRandomSeeds)
-        {
-            numRuns = 1; //no need for more runs since fixed seed
-        }
-        else{
+        if (!useRandomSeeds) {
+            numRuns = 1; // no need for more runs since fixed seed
+        } else {
             numRuns = runs;
         }
         Random r = new Random();
 
         // Create a thread pool with N threads (adjust based on CPU cores)
         int numThreads = Runtime.getRuntime().availableProcessors();
-        ExecutorService executor = Executors.newFixedThreadPool(numThreads-1);
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads - 1);
 
         for (int k = 0; k < numRuns; k++) {
 
             final long s;
-            if(useRandomSeeds)
-            {
+            if (useRandomSeeds) {
                 s = r.nextLong();
-            }
-            else{
+            } else {
                 s = seed;
             }
 
-                        final int it = k;
             final String chartName = "TRAINING_Batch_" + batchSize + "_HS_" + hiddenSize
                     + "_LR_" + learningRate + "_Seed_" + s;
-//System.out.println("Training..." + k);
+            // System.out.println("Training..." + k);
             executor.submit(() -> {
                 try {
-                 //   System.out.println("Thread handled");
-                  //  System.out.println(System.getProperty("user.dir") +"/myapp/"+ "src/data/BTC_train.csv");
-                    MLP mlp = new MLP(System.getProperty("user.dir") +"/myapp/"+ "src/data/BTC_train.csv", hiddenSize, hiddenLayers, 5, s,
+                    // System.out.println("Thread handled");
+                    // System.out.println(System.getProperty("user.dir") +"/myapp/"+
+                    // "src/data/BTC_train.csv");
+                    MLP mlp = new MLP(System.getProperty("user.dir") + "/myapp/" + "src/data/BTC_train.csv", hiddenSize,
+                            hiddenLayers, 5, s,
                             learningRate);
                     mlp.trainNetwork(iterations, batchSize, 50, 0.01);
 
@@ -66,8 +63,7 @@ public class AlgoFunctions {
                         mlp.saveModel(accuracy + " : " + chartName + ".mlp");
                         System.out.println("Finished: " + chartName);
 
-                        //mlp.testModel
-
+                        // mlp.testModel
 
                     }
                 } catch (Exception e) {
@@ -75,33 +71,29 @@ public class AlgoFunctions {
                     e.printStackTrace();
                 }
                 int progress = completed.incrementAndGet();
-        System.out.println("Iteration " + progress + " / " + numRuns);
+                System.out.println("Iteration " + progress + " / " + numRuns);
 
             });
         }
 
-try {
-    executor.shutdown();
-    return executor.awaitTermination(1, TimeUnit.HOURS);
-} catch (InterruptedException e) {
-    e.printStackTrace();
-    return false;
-}
+        try {
+            executor.shutdown();
+            return executor.awaitTermination(1, TimeUnit.HOURS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return false;
+        }
 
     }
 
-    public void testMLP(String filename)
-    {
-        try
-        {
-           // System.out.println(filename);
+    public void testMLP(String filename) {
+        try {
+            // System.out.println(filename);
             MLP mlp = MLP.loadModel(System.getProperty("user.dir") + "/" + filename);
-         //   System.out.println("Load model");
+            // System.out.println("Load model");
             double accuracy = mlp.testNetwork();
             System.out.println("Accuracy " + accuracy);
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- myapp/src/main/java/com/Graph.java
+++ myapp/src/main/java/com/Graph.java
@@ -8,11 +8,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-
-
-import java.io.File;
-import java.io.FileReader;
-import java.lang.reflect.Type;
 import java.util.*;
 
 public class Graph {
